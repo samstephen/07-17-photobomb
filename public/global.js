2857c30946c6d80eb00a4ea1d65fb7394e1df5f7
Sam Stephen <samueljstephen@gmail.com>
2015-07-18 17:46:05 -0500

    logo text takes user home

--- public/global.js
+++ public/global.js
@@ -1,16 +1,5 @@
-$(document.body).on("click", "h1#logo", function(){
-    var req = new XMLHttpRequest();
-    var navigation = document.getElementsByTagName('nav');
-    req.open('get', '/home');
-    req.send();
-})
-
+function goHome() {
+    window.location.replace("/home");
+}
 var logo = document.getElementById('logo');
 logo.addEventListener('click', goHome);
-
-function goHome() {
-    var req = new XMLHttpRequest();
-    var navigation = document.getElementsByTagName('nav');
-    req.open('get', '/home');
-    req.send();
-}
