fdea14183cc2c4a39033d3c95544feb9373cd806
Sam Stephen <samueljstephen@gmail.com>
2015-07-18 18:41:30 -0500

    show/hide form using js

--- public/global.js
+++ public/global.js
@@ -3,3 +3,18 @@ function goHome() {
 }
 var logo = document.getElementById('logo');
 logo.addEventListener('click', goHome);
+
+function showModal(event) {
+    event.preventDefault();
+    document.getElementById('add-user-form').style.visibility = 'visible';
+}
+
+var link = document.getElementById('add-user-link');
+link.addEventListener('click', showModal);
+
+function hideModal() {
+    document.getElementById('add-user-form').style.visibility = 'hidden';
+}
+
+var close = document.getElementById('close');
+close.addEventListener('click', hideModal);
