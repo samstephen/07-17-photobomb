1a9891a1121a93b91c597dcda586ba59cfb96965
Sam Stephen <samueljstephen@gmail.com>
2015-07-18 17:16:36 -0500

    WIP adding event listener to logo text to take user home

--- public/global.js
+++ public/global.js
@@ -1,3 +1,10 @@
+$(document.body).on("click", "h1#logo", function(){
+    var req = new XMLHttpRequest();
+    var navigation = document.getElementsByTagName('nav');
+    req.open('get', '/home');
+    req.send();
+})
+
 var logo = document.getElementById('logo');
 logo.addEventListener('click', goHome);
 
